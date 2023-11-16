ad166655584db941966fdc6a1758751a2cb1dae9
Connor Murphy <connor.murphy@duke.edu>
2023-11-15 19:45:02 -0500

    fixed function for testing methods, enabled testing for errors

--- Test.py
+++ Test.py
@@ -10,7 +10,27 @@ def test_func(f, x, y):
         - whether f(x) == y (bool)
         - return value of f called on the inputs in x 
     """
-    return f(*x) == y, f(*x)
+    try:
+        f_x = f(*x)
+    except Exception as e:
+        f_x = type(e)
+    return f_x == y, f_x
+
+
+def test_method(m, o, x, y):
+    """
+    tests the return value of a method m given an object o,
+    a list of inputs x, and expected output y.
+    returns a tuple containing:
+        - whether o.m(x) == y (bool)
+        - return value of m called on o and the inputs in x
+    NB: does not test whether m correctly handles side effects
+    """
+    try:
+        om_x = getattr(o, m.__name__)(*x)
+    except Exception as e:
+        om_x = type(e)
+    return om_x == y, om_x
 
 
 def create_case_dict(cases: list, names: dict):
@@ -36,6 +56,29 @@ def print_test_group(d: dict, i: int):
     """
     if i in d:
         print(d[i])
+
+
+def run_func_tests(cases: list, names: dict, v: bool):
+    d = create_case_dict(cases, names)
+    for i in range(len(cases)):
+        print_test_group(d, i)
+        passed, result = test_func(cases[i][0], cases[i][1], cases[i][2])
+        if not v:
+            print(f"\tTest {i}: {'Passed' if passed else f'Failed (expected {cases[i][2]}, got {result})'}")
+        else:
+            print(f"\tTest {i}: {'Passed' if passed else 'Failed'} (expected {cases[i][2]}, got {result})")
+
+
+def run_method_tests(cases: list, names: dict, v: bool):
+    d = create_case_dict(cases, names)
+    for i in range(len(cases)):
+        print_test_group(d, i)
+        passed, result = test_method(cases[i][0], cases[i][1], cases[i][2], cases[i][3])
+        if not v:
+            print(f"\tTest {i}: {'Passed' if passed else f'Failed (expected {cases[i][3]}, got {result})'}")
+        else:
+            print(f"\tTest {i}: {'Passed' if passed else 'Failed'} (expected {cases[i][3]}, got {result})")
+
 
 
 def test_vector_ops(v=False):
@@ -73,10 +116,14 @@ def test_vector_ops(v=False):
         (O.add, [vec[2], vec[3]], vec[8]),
         (O.add, [vec[4], vec[5]], V.Vector(1, [14])),
         (O.add, [vec[2], vec[2]], V.Vector(3, [34, 0, 2])),
+        (O.add, [vec[1], vec[2]], ValueError),
+        (O.add, [vec[4], vec[8]], ValueError),
 
         (O.sub, [vec[0], vec[1]], V.Vector(4, [3, 2, -6, -5])),
         (O.sub, [vec[2], vec[3]], vec[7]),
         (O.sub, [vec[4], vec[5]], V.Vector(1, [-2])),
+        (O.sub, [vec[0], vec[2]], ValueError),
+        (O.sub, [vec[9], vec[8]], ValueError),
 
         (O.eq, [vec[1], vec[6]], True),
         (O.eq, [vec[1], vec[0]], False),
@@ -91,17 +138,66 @@ def test_vector_ops(v=False):
         (O.ne, [vec[2]-vec[2], vec[8]], False),
     ]
 
-    d = create_case_dict(cases, names)
+    run_func_tests(cases, names, v)
 
-    for i in range(len(cases)):
-        print_test_group(d, i)
-        passed, result = test_func(cases[i][0], cases[i][1], cases[i][2])
-        if not v:
-            print(f"\tTest {i}: {'Passed' if passed else f'Failed (expected {cases[i][2]}, got {result})'}")
-        else:
-            print(f"\tTest {i}: {'Passed' if passed else 'Failed'} (expected {cases[i][2]}, got {result})")
 
+def test_vector_methods(v=False):
+    """
+    Tests vector methods: .times(), .dot(), .mag(), .at(), and .copy().
+    """
+    print(f"Testing vector methods:{' (verbose feedback)' if v else ''}")
     
+    vec = [
+        V.Vector(4, [4, 5, 1, 2]),  # 0
+        V.Vector(4, [1, 3, 7, 7]),
+        V.Vector(3, [17, 0, 1]),
+        V.Vector(3, [-17, 0, -1]),
+
+        V.Vector(1, [6]),           # 4
+        V.Vector(1, [8]),
+        V.Vector(4, [1, 3, 7, 7]),
+        V.Vector(3, [34, 0, 2]),
+
+        V.Vector(3),                # 8
+        V.Vector(4)
+    ]
+
+    names = {
+        V.Vector.times : "\n\tScalar multiplication (times):",
+        V.Vector.dot   : "\n\tInner product (dot):",
+        V.Vector.mag   : "\n\tMagnitude (mag):",
+        V.Vector.at    : "\n\tGet nth element (at):",
+        V.Vector.copy  : "\n\tCreate deep copy (copy):"
+    }
+
+
+    cases = [   # (method, object, [inputs], expected)
+        (V.Vector.times, vec[0], [3], V.Vector(4, [12, 15, 3, 6])),
+        (V.Vector.times, vec[0], [2], V.Vector(4, [8, 10, 2, 4])),
+        (V.Vector.times, vec[2], [1], vec[2]),
+        (V.Vector.times, vec[2], [0], vec[8]),
+
+        # (O.sub, [vec[0], vec[1]], V.Vector(4, [3, 2, -6, -5])),
+        # (O.sub, [vec[2], vec[3]], vec[7]),
+        # (O.sub, [vec[4], vec[5]], V.Vector(1, [-2])),
+
+        # (O.eq, [vec[1], vec[6]], True),
+        # (O.eq, [vec[1], vec[0]], False),
+        # (O.eq, [vec[0], vec[2]], False),
+        # (O.eq, [vec[2]+vec[2], vec[7]], True),
+        # (O.eq, [vec[2]-vec[2], vec[8]], True),
+
+        # (O.ne, [vec[1], vec[6]], False),
+        # (O.ne, [vec[1], vec[0]], True),
+        # (O.ne, [vec[0], vec[2]], True),
+        # (O.ne, [vec[2]+vec[2], vec[7]], False),
+        # (O.ne, [vec[2]-vec[2], vec[8]], False),
+    ]
+
+    run_method_tests(cases, names, v)
+
+
 
 if __name__ == "__main__":
     test_vector_ops()
+    # test_vector_methods()
